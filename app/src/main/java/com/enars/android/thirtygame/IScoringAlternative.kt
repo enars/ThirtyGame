3ed25dfa2f558be1b924733d09a9e6f63d18dde9
enars <ejnar.sorensen@gmail.com>
2020-08-05 13:21:30 +0200

    Rewrite Lows scoringAlternative

--- app/src/main/java/com/enars/android/thirtygame/IScoringAlternative.kt
+++ app/src/main/java/com/enars/android/thirtygame/IScoringAlternative.kt
@@ -3,6 +3,8 @@ package com.enars.android.thirtygame
 interface IScoringAlternative {
     val name: String
     fun calculateScore(dices: Array<Die>): Int
+    fun sum(dices: Array<Die>): Int {
+        return dices.map {d -> d.value}.reduce { acc, v -> acc + v} }
 }
 
 /**
@@ -17,18 +19,24 @@ interface IScoringAlternative {
 class Lows : IScoringAlternative {
     override val name = "Lows"
     override fun calculateScore(dices: Array<Die>): Int {
-        var dices = dices.filter { d -> d.value <= 3 }
-        val score: Int = dices.count()
-        return score
+        //var dices = dices.filter { d -> d.value <= 3 }
+
+        //if (dices.forEach { d -> return d.value >= 3 }) Check if invalid
+        return dices.map { die -> die.value }
+                    .filter { d -> d <= 3 }
+                    .reduce { acc, d -> acc + d }
     }
 }
 
 class Fours : IScoringAlternative {
     override val name = "Fours"
     override fun calculateScore(dices: Array<Die>): Int {
-        val sum = dices.map { d -> d.value }.sum()
-        val score = sum / 4
-        return score
+        val sum = dices.map {d -> d.value}
+                            .reduce { acc, v -> acc + v}
+        when (sum % 4 ) {
+            0 -> return sum
+            else -> return -1
+        }
     }
 }
 
