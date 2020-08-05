57ad08d24433b49e667cf662f4e50b38ad297c75
enars <ejnar.sorensen@gmail.com>
2020-08-05 13:46:03 +0200

    Clean up and add descriptions

--- app/src/main/java/com/enars/android/thirtygame/IScoringAlternative.kt
+++ app/src/main/java/com/enars/android/thirtygame/IScoringAlternative.kt
@@ -1,5 +1,11 @@
 package com.enars.android.thirtygame
 
+/**
+ * Scoring alternatives are the objects visible in the dropdown menu.
+ * Depending on what alternative was chosen, the dies are scored differently.
+ * All ScoringAlternatives except the one called "Lows" use the initial interface method for
+ * calculation.
+ */
 interface IScoringAlternative {
     val name: String
     val value: Int
@@ -14,19 +20,25 @@ interface IScoringAlternative {
         return dices.map {d -> d.value}.reduce { acc, v -> acc + v} }
 }
 
+/**
+ * Lows overrides the initial scoring method.
+ * Scores every die that has the value 3 or lower by filtering out 4+ and summing the remaining dies
+ */
 class Lows : IScoringAlternative {
     override val name = "Lows"
     override val value = 3
     override fun calculateScore(dices: Array<Die>): Int {
-        //var dices = dices.filter { d -> d.value <= 3 }
-
-        //if (dices.forEach { d -> return d.value >= 3 }) Check if invalid
         return dices.map { die -> die.value }
                     .filter { d -> d <= 3 }
                     .reduce { acc, d -> acc + d }
     }
 }
 
+/**
+ * The following "Fours" to "Twelves" alternatives use the top level method for scoring.
+ * They sum all dies and checks if the <sum modulus(%) scoring value> equals 0, i.e. if it is
+ * cleanly divisible by the "value" variable from the scoring alternative.
+ */
 class Fours : IScoringAlternative {
     override val name = "Fours"
     override val value = 4
